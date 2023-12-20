5cbba40626e5c3e27834e70e5cbaa851067bb766
Elvisthegreat <>
2023-12-20 15:54:15 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+# Ask the user for their name
+def ask_user_for_name():
+
+    print('Initializing start game in process...')
+    str_name = input('Enter Name')
+    print(f'Thanks {name} for the name.')
+ask_user_for_name(name)
