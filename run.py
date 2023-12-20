1bd3336d8223d3b123f3c4ee740e21c01374d65e
Elvisthegreat <>
2023-12-20 16:39:50 +0000

    Add a loading game function to load from 10 to 1

--- run.py
+++ run.py
@@ -2,7 +2,19 @@ import random
 # Ask the user for their name
 def ask_user_for_name():
 
-    print('Initializing start game in process...')
-    str_name = input('Enter Name')
-    print(f'Thanks {name} for the name.')
-ask_user_for_name(name)
+    print('Initializing...! Starting game in process...')
+    user_name = input('Please enter a Name: ')
+    return f"Thank you, {user_name} for your name!"
+    
+return_name = ask_user_for_name()
+print(return_name)
+
+def loading_game_():
+    print("Loading Game.....")
+
+    loading_game = 10
+    while loading_game > 0:
+        print(f"{loading_game} seconds... ")
+        loading_game -=1
+    print('Loading Completed')
+loading_game_()
