78e002f87d5922a37300e259962adba58d11dcfa
Elvisthegreat <>
2023-12-21 16:52:07 +0000

    Add the guessing number function from 0 to 5

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def ask_user_for_name():
     """
     print('Initializing...! Starting game in process...')
     user_name = input('Please enter a Name: ')
-    return f"Thank you, {user_name} for your name!"
+    return f"Thank you, {user_name} for your name!\n"
     
 return_name = ask_user_for_name()
 print(return_name)
@@ -20,14 +20,31 @@ def loading_game_():
     while loading_game > -1:
         print(f"{loading_game} seconds...\n ")
         loading_game -=1
-    print('Loading Completed')
+    print('Loading Completed\n')
 loading_game_()
 
 """
 Main game board section, starting from 5 to 30 
 """
 def guessing_game_step_one():
-    # Guessing from 1 to 5 with 2 chances trial
-    print("I'm thinking of a number from 0 to 5, can you can what number it is? ")
-    guessing_number = 0
+    # Guessing from 0 to 5 with 2 chances trial
+    print("I'm thinking of a number from 0 to 5, can you guess what number it is?\n ")
+
+    guessing_number = random.randint(0, 5)
     trial = 2
+
+    while trial > 0:
+
+        guessing_the_number = int(input('Can you try and guess?'))
+        
+        if guessing_the_number == guessing_number:
+            print(f"Well done! Nice guessing")
+            return
+        else:
+            trial-=1
+            if trial > 0:
+                print(f"No, You have {trial} more trial(s) to guess the number.\n")
+            else:
+                print(f"Sorry, you have run out of trials. The number I was thinking of was {guessing_number}.")
+                
+guessing_game_step_one()
